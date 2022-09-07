530fbbe7d6fe9bd4c4c3147816e659da07cd9bd5
Zaki <Zaki@ZAKI>
2022-09-07 07:53:55 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace At_Barika_F
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+           
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+                
+                
+            }
+        }
+        bool IsMaximized = false;
+        
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ClickCount == 2)
+            {
+
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1250;
+                    this.Height = 810;
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    IsMaximized = true;
+
+                }
+            }
+
+        }
+
+        private void Border_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        
+        
+        private void Call(object sender, RoutedEventArgs e)
+        {
+            Frm.Content = new Inscription();
+            var selected = sender as ListViewItem;
+            String s = selected.Name;
+            switch(s)
+            {
+                case "Subscribe":
+                    Frm.Content = new Inscription();
+                    break;
+                case "Offre":
+                    Frm.Content = new Promotion();
+                    break;
+                case "Transform":
+                    Frm.Content = new Transform();
+                    break;
+
+            }
+
+
+
+        }
+
+       
+    }
+}
---
+++ Transform.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace At_Barika_F
+{
+    /// <summary>
+    /// Interaction logic for Transform.xaml
+    /// </summary>
+    public partial class Transform : UserControl
+    {
+        public Transform()
+        {
+            InitializeComponent();
+
+            var converter = new BrushConverter();
+            ObservableCollection<Transformdata> transform =new ObservableCollection<Transformdata>();
+        }
+
+
+        public class Transformdata
+        {
+             public int IDTransform { get; set; }
+            public string IDClient { get; set; }
+            public int IDLine { get; set; }
+            public string OldStreet { get; set; }
+            public string NewStreet { get; set; }
+
+        }
+
+    }
+}
